f5b9012c21dc3215d18e0ac9f93deadc6c3f8de0
JoshSmile <33873286+JoshSmile@users.noreply.github.com>
2019-10-01 14:34:41 +0100

    Add files via upload
    ## Installation
    1. Copy downloaded mstscfix.ps1, to the root of your PCs C: drive
    2. Close any Remote Apps or RDP sessions, ensure mstsc.exe is not running under task manager
    3. Search for & Run: Windows Powershell as administrator (right click to run as administrator)
    4. Run: Set-ExecutionPolicy Unrestricted
    5. Select: A
    6. Run: cd /
    7. Run: & '.\mstscfix.ps1'
    8. Run: Set-ExecutionPolicy restricted
    9. Close Windows Powershell

---
+++ mstscundo.ps1
@@ -0,0 +1,30 @@
+﻿#taking ownship of files to change permission
+echo "Taking Ownership of files"
+takeown /f "C:\Windows\System32\mstsc.exe"
+takeown /f "C:\Windows\System32\mstscax.dll"
+
+
+#Applying permission changed to rename file.
+echo "Giving permission over files"
+$rule=new-object System.Security.AccessControl.FileSystemAccessRule ("$env:userdomain\$env:username","FullControl","Allow")
+$acl = Get-ACL C:\Windows\System32\mstsc.exe
+$acl.SetAccessRule($rule)
+Set-ACL -Path C:\Windows\System32\mstsc.exe -AclObject $acl
+
+
+$rule=new-object System.Security.AccessControl.FileSystemAccessRule ("$env:userdomain\$env:username","FullControl","Allow")
+$acl = Get-ACL C:\Windows\System32\mstscax.dll
+$acl.SetAccessRule($rule)
+Set-ACL -Path C:\Windows\System32\mstscax.dll -AclObject $acl
+
+#Rename Files
+ren C:\Windows\System32\mstsc.exe mstsc.exe.1703
+ren C:\Windows\System32\mstscax.dll mstscax.dll.1703
+
+#Rename Files
+ren C:\Windows\System32\mstsc.exe.bak mstsc.exe
+ren C:\Windows\System32\mstscax.dll.bak mstscax.dll
+
+#Giving Ownership back to file.
+icacls "C:\Windows\System32\mstsc.exe" /setowner "NT Service\TrustedInstaller"
+icacls "C:\Windows\System32\mstscax.dll" /setowner "NT Service\TrustedInstaller"
